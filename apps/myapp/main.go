f9e15da552dbd14fde06023e3363fe0623b30f23
linlife <1521052712@qq.com>
2025-08-31 05:26:31 +0800

    go update

--- apps/myapp/main.go
+++ apps/myapp/main.go
@@ -5,9 +5,11 @@ import (
   "log"
   "net/http"
   "os"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+  http.Handle("/metrics", promhttp.Handler())
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     host, _ := os.Hostname()
     fmt.Fprintf(w, "hello from myapp,nice to see you! | host=%s | path=%s\n", host, r.URL.Path)
